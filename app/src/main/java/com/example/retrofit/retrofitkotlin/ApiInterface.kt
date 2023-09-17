47c5e99aec1c47ab3dac15afe982b31b29b0dcc5
Husnain <husnainalirafique125@gmail.com>
2023-09-17 07:19:32 +0500

    retrofit

--- app/src/main/java/com/example/retrofit/retrofitkotlin/ApiInterface.kt
+++ app/src/main/java/com/example/retrofit/retrofitkotlin/ApiInterface.kt
@@ -1,5 +1,9 @@
 package com.example.retrofit.retrofitkotlin
 
-interface ApiInterface {
+import retrofit2.Call
+import retrofit2.http.GET
 
+interface ApiInterface {
+    @GET("gimme")
+    fun getData(): Call<ResponseDataClass>
 }
